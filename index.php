0bca787ab8472106606b5d1982923a5346cf5eda
yuriya <namyuri@gmail.com>
2019-04-25 23:22:09 +0900

    cleanup template

--- index.php
+++ index.php
@@ -1,3 +1,76 @@
-<?php
-    echo phpversion();
-?>
+<!DOCTYPE html>
+<html>
+    <title>영락교회 갈보리 찬양대</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>
+        * {font-family: "Raleway", sans-serif}
+    </style>
+    <body class="w3-light-grey">
+        <!-- Top container -->
+        <?php include "./php_lib/top_menu.php"; ?>
+
+        <!-- Sidebar/menu -->
+        <nav class="w3-sidebar w3-collapse w3-white w3-animate-left" style="z-index:3;width:300px;" id="mySidebar"><br>
+        <div class="w3-container w3-row">
+            <div class="w3-col s4">
+                <img src="./img/avatar2.png" class="w3-circle w3-margin-right" style="width:46px">
+            </div>
+            <div class="w3-col s8 w3-bar">
+                로그인 하세요
+            </div>
+        </div>
+        <hr>
+        <div class="w3-container">
+            <h5>메뉴</h5>
+        </div>
+        <div class="w3-bar-block">
+            <a href="#" class="w3-bar-item w3-button w3-padding-16 w3-hide-large w3-dark-grey w3-hover-black" onclick="w3_close()" title="close menu"><i class="fa fa-remove fa-fw"></i>  메뉴 닫기</a>
+            <a href="./index.php" class="w3-bar-item w3-button w3-padding"><i class="fa fa-home"></i>  홈페이지</a>
+            <a href="#" class="w3-bar-item w3-button w3-padding"><i class="fa fa-users fa-fw"></i>  출석입력</a>
+            <a href="#" class="w3-bar-item w3-button w3-padding"><i class="fa fa-eye fa-fw"></i>  출석현황</a>
+            <a href="#" class="w3-bar-item w3-button w3-padding"><i class="fa fa-bar-chart"></i>  출석통계</a>
+            <a href="#" class="w3-bar-item w3-button w3-padding"><i class="fa fa-cog"></i>  관리자</a><br><br>
+        </div>
+        </nav>
+
+
+        <!-- Overlay effect when opening sidebar on small screens -->
+        <div class="w3-overlay w3-hide-large w3-animate-opacity" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
+
+        <!-- !PAGE CONTENT! -->
+        <div class="w3-main" style="margin-left:300px;margin-top:43px;">
+
+        </div>
+        <!-- !END PAGE CONTENT! -->
+
+        <script>
+            // Get the Sidebar
+            var mySidebar = document.getElementById("mySidebar");
+
+            // Get the DIV with overlay effect
+            var overlayBg = document.getElementById("myOverlay");
+
+            // Toggle between showing and hiding the sidebar, and add overlay effect
+            function w3_open() {
+            if (mySidebar.style.display === 'block') {
+                mySidebar.style.display = 'none';
+                overlayBg.style.display = "none";
+            } else {
+                mySidebar.style.display = 'block';
+                overlayBg.style.display = "block";
+            }
+            }
+
+            // Close the sidebar with the close button
+            function w3_close() {
+            mySidebar.style.display = "none";
+            overlayBg.style.display = "none";
+            }
+        </script>
+
+    </body>
+</html>
