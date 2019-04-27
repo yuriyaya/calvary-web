c22a2cd0526ad1fb5788d182961ecaf0b9047bd1
yuriya <namyuri@gmail.com>
2019-04-27 12:07:57 +0900

    add login ui

--- index.php
+++ index.php
@@ -16,12 +16,7 @@
         <!-- Sidebar/menu -->
         <nav class="w3-sidebar w3-collapse w3-white w3-animate-left" style="z-index:3;width:300px;" id="mySidebar"><br>
         <div class="w3-container w3-row">
-            <div class="w3-col s4">
-                <img src="./img/avatar2.png" class="w3-circle w3-margin-right" style="width:46px">
-            </div>
-            <div class="w3-col s8 w3-bar">
-                로그인 하세요
-            </div>
+            <button onclick="document.getElementById('id01').style.display='block'" class="w3-button w3-green w3-large">로그인</button>
         </div>
         <hr>
         <div class="w3-container">
@@ -36,10 +31,42 @@
             <a href="#" class="w3-bar-item w3-button w3-padding"><i class="fa fa-cog"></i>  관리자</a><br><br>
         </div>
         </nav>
-
-
         <!-- Overlay effect when opening sidebar on small screens -->
         <div class="w3-overlay w3-hide-large w3-animate-opacity" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
+
+        <!-- log in modal form -->
+        <div id="id01" class="w3-modal">
+            <div class="w3-modal-content w3-card-4 w3-animate-zoom" style="max-width:600px">
+                <div class="w3-center"><br>
+                    <span onclick="document.getElementById('id01').style.display='none'" class="w3-button w3-xlarge w3-hover-red w3-display-topright" title="Close Modal">&times;</span>
+                </div>
+
+                <form class="w3-container" action="/action_page.php">
+                    <div class="w3-section">
+                        <label><b>아이디</b></label>
+                        <select id="login_id" name="login_id" onchange="changeSelectedId()">
+                            <option value="" selected disabled>파트를 선택</option>  
+                            <option value="sopa">소프라노A</option>
+                            <option value="sopb">소프라노B</option>
+                            <option value="sopbp">소프라노B+</option>
+                            <option value="altoa">알토A</option>
+                            <option value="altob">알토B</option>
+                            <option value="tenner">테너</option>
+                            <option value="bass">베이스</option>
+                            <option value="operator">임원단</option>
+                            <option value="admin">Admin</option>
+                        </select>
+                        <input class="w3-input w3-border w3-margin-bottom" type="text" id="username" placeholder="아이디를 입력하세요" name="usrname" required>
+                        <label><b>비밀번호</b></label>
+                        <input class="w3-input w3-border" type="password" placeholder="비밀번호를 입력하세요" name="psw" required>
+                        <button class="w3-button w3-block w3-green w3-section w3-padding" type="submit">Login</button>
+                    </div>
+                </form>
+                <div class="w3-container w3-border-top w3-padding-16 w3-light-grey">
+                    <button onclick="document.getElementById('id01').style.display='none'" type="button" class="w3-button w3-red">Cancel</button>
+                </div>
+            </div>
+        </div>
 
         <!-- !PAGE CONTENT! -->
         <div class="w3-main" style="margin-left:300px;margin-top:43px;">
@@ -50,10 +77,8 @@
         <script>
             // Get the Sidebar
             var mySidebar = document.getElementById("mySidebar");
-
             // Get the DIV with overlay effect
             var overlayBg = document.getElementById("myOverlay");
-
             // Toggle between showing and hiding the sidebar, and add overlay effect
             function w3_open() {
             if (mySidebar.style.display === 'block') {
@@ -64,12 +89,21 @@
                 overlayBg.style.display = "block";
             }
             }
-
             // Close the sidebar with the close button
             function w3_close() {
-            mySidebar.style.display = "none";
-            overlayBg.style.display = "none";
+                mySidebar.style.display = "none";
+                overlayBg.style.display = "none";
             }
+
+            function changeSelectedId(){
+                var selectedLoginId = document.getElementById("login_id");
+                var selectValue = selectedLoginId.options[selectedLoginId.selectedIndex].value;
+                var selectText = selectedLoginId.options[selectedLoginId.selectedIndex].text;
+                // alert(selectValue);
+                // alert(selectText);
+                document.getElementById("username").value = selectValue;
+            }
+
         </script>
 
     </body>
