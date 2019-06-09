880562b04d6a129d194c8fbe39ba8e57905b4fd3
yuriya <namyuri@gmail.com>
2019-06-09 20:32:38 +0900

    operator page : replace 신입/제적대원조회 to 대원변동사항조회, more general function

--- operator_member_stat_search.php
+++ operator_member_stat_search.php
@@ -28,24 +28,33 @@
                 if(isset($_SESSION['u_id'])) {
             
                     if(($_SESSION['u_id'] == 'operator') || ($_SESSION['u_id'] == 'admin')) {
-                        include_once "./inc/operator_menu.php";
-                        include_once "./inc/display_attendence.php";
-                        if(isset($_POST['report_member_state_search'])) {
-                            $member_state_date = $_POST['member_state_date'];
-
-                            $date_start = date('Y-m', strtotime($member_state_date)).'-01';
-                            $date_end = date('Y-m-t', strtotime($member_state_date));
+                        include_once "./inc/admin_menu.php";
+                        
+                        if(isset($_POST['search_member_stat_update_submit'])) {
+                            $date_start = $_POST['search_date_start'];
+                            $date_end = $_POST['search_date_end'];
+                            // echo $date_start.'<br>';
+                            // echo $date_end.'<br>';
 
                             include './inc/dbconn.php';
-                            $query = "SELECT mi.part, mi.name, ms.state_update_date, ms.state FROM member_state AS ms RIGHT JOIN member_info AS mi ON ms.id=mi.id WHERE state_update_date>='".$date_start."' AND state_update_date<='".$date_end."' AND (ms.state=3 OR ms.state=7) ORDER BY state_update_date DESC;";
+                            $query = "SELECT ms.id, mi.name, ms.state_update_date, ms.state FROM member_state AS ms LEFT JOIN member_info AS mi ON ms.id=mi.id WHERE state_update_date>='".$date_start."' AND state_update_date<='".$date_end."' ORDER BY id ASC, state_update_date DESC;";
                             $stmt = $conn->prepare($query);
                             $stmt->execute();
                             $stmt->setFetchMode(PDO::FETCH_ASSOC);
-                            $result_html = '<table class="w3-table-all w3-hoverable"><tr><th>파트</th><th>이름</th><th>변동일</th><th>상태</th></tr>';
+
+                            $search_result_info = '<table class="w3-table-all w3-hoverable"><tr><th>파트</th><th>이름</th><th>변동일</th><th>변동사항</th></tr>';
+
                             while($row = $stmt->fetch()) {
-                                $result_html = $result_html.'<tr><td>'.returnPartName($row['part']).'</td><td>'.$row['name'].'</td><td>'.$row['state_update_date'].'</td><td>'.getMemberStateString($row['state']).'</td></tr>';
+                                $search_result_info = $search_result_info.'<tr><td>';
+                                $part_num = (int)($row['id']/10000);
+                                $search_result_info = $search_result_info.returnPartName($part_num).'</td><td>';
+                                $search_result_info = $search_result_info.$row['name'].'</td><td>';
+                                $search_result_info = $search_result_info.$row['state_update_date'].'</td><td>';
+                                $search_result_info = $search_result_info.getMemberStateString($row['state']).'</td></tr>';
                             }
-                            $result_html = $result_html.'</table>';
+
+                            $search_result_info = $search_result_info.'</table>';
+
                         }
                     } else {
                         $status_msg_code = '5003';
@@ -59,22 +68,25 @@
                     $status_msg_code = '';
                 }
             ?>
-            <div class="operator_report_member_state">
-                <form class="operator_report_member_state_form" action="<?=$_SERVER['PHP_SELF']?>" method="POST">
+            <!-- !ALERT! -->
+            <div class="search_member_stat_update">
+                <form class="search_member_stat_update_form" action="<?=$_SERVER['PHP_SELF']?>" method="POST">
                     <table style="border:0px;">
                         <tr>
-                            <td>조회 일자 : </td><td><input type="date" name="member_state_date" value="<?php if(empty($member_state_date)){echo date("Y-m-d", strtotime("-1 months"));} else {echo $member_state_date;} ?>"></td>
+                            <td>조회 기간 : </td><td><input type="date" name="search_date_start" value="<?php if(empty($date_start)){echo date('Y-m').'-01';} else {echo $date_start;} ?>"></td>
+                            <td> ~ <input type="date" name="search_date_end" value="<?php if(empty($date_end)){echo date('Y-m-t');} else {echo $date_end;} ?>"></td>
                         </tr>
                         <tr>
                             <td></td>
                             <td>
-                                <button type="submit" name="report_member_state_search" class="w3-button w3-green">조회</button>
+                                <button type="submit" name="search_member_stat_update_submit" class="w3-button w3-green">조회</button>
                             </td>
                         </tr>
                     </table>
                 </form>
-                <?php if(!empty($result_html)){echo $result_html;} ?>
             </div>
+            <?php if(!empty($search_result_info)){echo $search_result_info;} ?>
+
         </div>
         <!-- !END PAGE CONTENT! -->
 
