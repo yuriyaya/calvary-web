4a3dbc14d4ab5ecd6d78891d9d680112cc61469c
yuriya <namyuri@gmail.com>
2019-11-30 15:26:13 +0900

    operator menu re-arrange(member and attendence stat)

--- inc/operator_menu.php
+++ inc/operator_menu.php
@@ -2,6 +2,8 @@
     <a href="operator_att_stat_day.php" class="w3-bar-item w3-button w3-mobile">일별출석통계</a>
     <a href="operator_attendence_log_monthly.php" class="w3-bar-item w3-button w3-mobile">월별출석통계</a>
     <a href="operator_att_stat_year.php" class="w3-bar-item w3-button w3-mobile">일년출석통계</a>
+    <a href="operator_soli_att_stat.php" class="w3-bar-item w3-button w3-mobile">솔리스트출석률</a>
+    <a href="operator_100_att_rate.php" class="w3-bar-item w3-button w3-mobile">월별개근대원</a>
     <a href="operator_report_stat.php" class="w3-bar-item w3-button w3-mobile">월말보고서 출석통계</a>
     <a href="operator_100_att_rate_year.php" class="w3-bar-item w3-button w3-mobile">일년 개근정근</a>
 </div>
